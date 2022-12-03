e5de68f655d72bac6950b17e55240d2a6199548d
cod3mos <contato@codemos.com.br>
2022-12-02 21:35:01 -0300

    feat: add index

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Application, Bootstrap, Process } from './application'
+export { ErrorException, NotFoundErrorException, NotImplementYetErrorException } from './common/exceptions'
+export { HttpMethodEnum } from './common/http-protocols/http-method-enum'
+export { first, isEmpty, isNotEmpty, isNotNull, isNull, last, not, throwIf } from './helpers'
+
