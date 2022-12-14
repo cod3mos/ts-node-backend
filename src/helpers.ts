2db18b1e0f75323282acc9e853d614ad9bc3fc2b
cod3mos <contato@codemos.com.br>
2022-12-14 11:42:08 -0300

    feat: add new helper functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -23,6 +23,18 @@ export function isNotEmpty (value: any): boolean {
   return not(isEmpty(value))
 }
 
+export function onlyDefined (items: any[]): Array<[string, any]> {
+  return items.filter(values => !values.includes(undefined))
+}
+
+export function keyIncluded (items: Record<string, any>, key: string): Boolean {
+  return Object.keys(items).includes(key)
+}
+
+export function valueIncluded (items: Record<string, any>, value: any): Boolean {
+  return Object.values(items).includes(value)
+}
+
 export function first<T = any> (items: T[]): T {
   return items[0]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Application, Bootstrap, Process } from './application'
 export { ErrorException, NotFoundErrorException, NotImplementYetErrorException } from './common/exceptions'
 export { HttpMethodEnum } from './common/http-protocols/http-method-enum'
-export { first, isEmpty, isNotEmpty, isNotNull, isNull, last, not, throwIf } from './helpers'
+export { first, isEmpty, isNotEmpty, isNotNull, isNull, keyIncluded, last, not, onlyDefined, throwIf, valueIncluded } from './helpers'
 
