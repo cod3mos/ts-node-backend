c5d3fb27bb059fb1d9effca0ef157d98f22cf158
cod3mos <contato@codemos.com.br>
2022-12-16 12:14:26 -0300

    fix: exported query params

--- src/common/http/api/index.ts
+++ src/common/http/api/index.ts
@@ -1,6 +1,6 @@
 export { API } from './api'
 export { APIConfig } from './api-config'
-export { APIRequest } from './api-request'
+export { APIRequest, QueryParams } from './api-request'
 export { APIRequestPayload } from './api-request-payload'
 export { APIResponse } from './api-response'
 export { PaginationQueryParams } from './query/pagination-query-params'
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Application, Bootstrap, Process } from './application'
 export { ErrorException, NotFoundErrorException, NotImplementYetErrorException } from './common/exceptions'
-export { API, APIConfig, APIRequest, APIRequestPayload, APIResponse, PaginationQueryParams, SortPaginationQueryParams } from './common/http/api'
+export { API, APIConfig, APIRequest, APIRequestPayload, APIResponse, PaginationQueryParams, QueryParams, SortPaginationQueryParams } from './common/http/api'
 export { HttpMethodEnum } from './common/http/protocols/http-method-enum'
 export { AxiosClient, AxiosClientResponse, ProviderFactory, ProvidersEnum } from './common/http/providers'
 export { first, isEmpty, isNotEmpty, isNotNull, isNull, keyIncluded, last, not, onlyDefined, throwIf, valueIncluded } from './helpers'
